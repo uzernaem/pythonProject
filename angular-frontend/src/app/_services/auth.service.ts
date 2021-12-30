10e3524e413de7c2a40c602d7cee051a9293dc00
Alexander Odintsov <uzernaem@gmail.com>
2021-12-30 11:49:41 +0500

    Server Url optimization

--- angular-frontend/src/app/_services/auth.service.ts
+++ angular-frontend/src/app/_services/auth.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { serverUrl } from '../_services/baseurl';
 
-const AUTH_API = 'http://localhost:8000/auth/';
+const AUTH_API = serverUrl+'auth/';
 
 const httpOptions = {
   headers: new HttpHeaders({ 'Content-Type': 'application/json' })
